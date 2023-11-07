371fcab0d267adc6e224ba1f11182f710d3bd2a5
matsuyama <aya.k.matsuyama@gmail.com>
2023-11-08 03:56:47 +0900

    データの保存先をJSONファイルからデータベースに切り替えた

--- app.rb
+++ app.rb
@@ -2,63 +2,34 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
-require 'json'
 require 'cgi'
+require 'pg'
 
-before do
-  File.open('model/data.json', 'r') do |file|
-    file_contents = file.read
-    @memos = JSON.parse(file_contents) unless file_contents.empty?
-  end
-end
+conn = PG.connect(dbname: 'memo_app')
+conn.exec('CREATE TABLE IF NOT EXISTS memos(id serial PRIMARY KEY, title text, content text)')
 
 helpers do
   def html_escape(string)
     CGI.escapeHTML(string)
   end
-
-  def write_data(memos)
-    File.open('model/data.json', 'w') do |file|
-      file.write(JSON.dump(memos))
-    end
-  end
 end
 
 get '/' do
+  result = conn.exec('SELECT * FROM memos')
+  @memos = result.map { |table_row| table_row }
   erb :index
 end
 
-def store_data
-  current_data = File.empty?('model/data.json') ? {} : @memos
-  new_memo = prepare_new_memo
-  updated_data = create_data(current_data, new_memo)
-  write_data(updated_data)
-end
-
-def prepare_new_memo
-  new_memo = { title: params[:title], content: params[:content] }
-  handle_empty_fields(new_memo)
-end
-
-def handle_empty_fields(new_memo)
-  # メモ生成時にタイトルと内容が入力されていなかった場合の対処
-  if [:title, :content].all? { new_memo[_1].empty? }
-    redirect to('/')
-  elsif new_memo[:title].empty?
-    new_memo[:title] = '無題'
-  end
-  new_memo
-end
-
-def create_data(current_data, new_memo)
-  # 生成されたメモに連番のIDを与える
-  id = current_data.any? ? current_data.keys.map(&:to_i).max + 1 : 1
-  current_data[id] = new_memo
-  current_data
-end
-
 post '/memos' do
-  store_data
+  title = params[:title]
+  content = params[:content]
+  if [title, content].all?(&:empty?)
+    redirect to('/')
+  elsif title.empty?
+    title = '無題'
+  end
+  sql = 'INSERT INTO memos(title, content) VALUES ($1, $2)'
+  conn.exec_params(sql, [title, content])
   redirect to('/')
 end
 
@@ -67,34 +38,33 @@ get '/memos/new' do
 end
 
 get '/memos/:id' do
-  @id = params[:id]
-  @memo = @memos[@id]
+  memo = conn.exec("SELECT * FROM memos WHERE id = '#{params[:id]}'")
+  @memo = memo[0]
+
   return erb :error if !@memo
 
   erb :show
 end
 
 delete '/memos/:id' do
-  id = params[:id]
-  @memos.delete(id)
-  write_data(@memos)
+  conn.exec("DELETE FROM memos WHERE id = #{params[:id]}")
+
   redirect to('/')
 end
 
 get '/memos/edit/:id' do
-  @id = params[:id]
-  @memo = @memos[@id]
+  memo = conn.exec("SELECT * FROM memos WHERE id = '#{params[:id]}'")
+  @memo = memo[0]
+
   return erb :error if !@memo
 
   erb :edit
 end
 
 patch '/memos/:id' do
-  id = params[:id]
-  memo = @memos[id]
-  memo['title'] = params[:title]
-  memo['content'] = params[:content]
-  write_data(@memos)
+  sql = "UPDATE memos SET(title, content) = ($1, $2) WHERE id = '#{params[:id]}'"
+  conn.exec_params(sql, [params[:title], params[:content]])
+
   redirect to('/')
 end
 
